e82949434e83c2acd24be0b5bc284a55f83833cf
billaprashanth <prashanthbilla590@gmail.com>
2023-09-21 11:41:25 +0530

    Moved Data

--- app_server/controllers/location.js
+++ app_server/controllers/location.js
@@ -1,9 +1,25 @@
 module.exports.about= function(req,res){
-    res.render('about',{title:'About'});
+    res.render('about',{
+        title: 'Food Hub',
+        pageHeader: {
+            title: 'Harland Sanders',
+            para : 'I am the co-founder of,',
+            titlepara:'Grill9 ',
+            listpara:'Restaurant and ',
+            finalpara:'Website Designer',
+        }
+    });
 };
 
 module.exports.login = function(req,res){
-    res.render('login',{title:'login'});
+    // res.render('login',{title:'login'});
+    res.render('login',{
+        title: 'Login Example',
+        pageHeader:{
+            title:'Welcome back',
+        }
+    }
+    );
 };
 
 module.exports.menu= function(req,res){
